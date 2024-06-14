c86a2295e503cec8406261f9197db390b56643b0
Dev_ZIKI <marciojjacometo@gmail.com>
2024-06-14 16:31:16 -0300

    Remover Incompleto

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Lista lista = new();
+
+lista.inserirInicio(10);
+lista.inserirInicio(7);
+lista.inserirInicio(12);
+lista.inserirInicio(-5);
+
+int valor = -7;
+No noAnterior, noAtual;
+noAnterior = null;
+noAtual = null;
+
+Boolean verif = lista.consulta(valor, ref noAtual, ref noAnterior);
+if(verif == true) Console.WriteLine("Número encontrado!");
+else Console.WriteLine("Número não encontrado!");
+
+lista.imprimir();
