82ec5c341594f5316f3f9291d21fc08ab6a2f84d
Dev_ZIKI <marciojjacometo@gmail.com>
2024-08-09 16:06:58 -0300

    Exercicio 3 implementado

--- Program.cs
+++ Program.cs
@@ -8,10 +8,19 @@ namespace Lista{
             lista.insertSort(-2);
             lista.insertSort(11);
             lista.insertEnd(-12);
+            Boolean Search = List.Search(int value, ref Node current, ref  Node before);
+            if(Search == true) Console.WriteLine("A busca encontrou!");
+            else Console.WriteLine("A busca não encontrou!");
             lista.Search(6);
             lista.remove(-2);
             lista.remove(-12);
-            // lista.insertEnd(2);
+            lista.showList();
+
+            //Ex 3
+            lista.insertSort("Matemática," + " Diurno," + " " + 3 + "Horas," + "Professor Bruno");
+            lista.insertSort("História," + " Matutino," + " " + 2 + "Horas," + "Professor Bernado");
+            lista.Search("Matemática");
+            lista.remove("História");
             lista.showList();
 
         }
